47e53396dfa42b38863e407e4b1a91f56b7480d9
Spencer Beck <spencerb936@gmail.com>
2020-11-15 14:24:43 -0500

    started web server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express' )
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+
+})
+
+app.get('/hi', (req, res) => {
+    res.send('get out')
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
